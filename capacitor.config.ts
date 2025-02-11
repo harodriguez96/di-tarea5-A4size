263708c559fc6511c27102d0e05e442509b16a33
di-birt <mardanza@birt.eus>
2025-02-11 11:58:39 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'DI04_TE01_v3',
+  webDir: 'www'
+};
+
+export default config;
